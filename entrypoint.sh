baa00e0e0e7f61897b5cebf739faec55f25d741d
loop963 <30581934+loop963@users.noreply.github.com>
2019-09-27 18:41:24 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "HTML5-based Speedtest on Docker"
+echo ""
+echo "Author : adolfintel"
+echo "Project : https://github.com/adolfintel/speedtest"
+echo ""
+echo "Enjoy your speed testing !"
+echo ""
+echo "[i] Starting Apache2 Foreground Mode ..."
+echo ""
+httpd -D FOREGROUND
